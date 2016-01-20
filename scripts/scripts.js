387a6077aef97d4072fe17153f89761f3bd60c06
John Harden <john.m.harden@gmail.com>
2016-01-19 19:57:28 -0800

    additional tweaks to about me section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,15 +22,21 @@ window.addEventListener("optimizedScroll", function() {
 
     if(window.scrollY >= document.getElementById('hero_banner').getBoundingClientRect().height
         - document.getElementById('hero_banner').getBoundingClientRect().height/2
-    && (window.scrollY <= document.getElementById('hero_banner').getBoundingClientRect().height + document.getElementById('about_me').getBoundingClientRect().height/2)
+    && (window.scrollY <=  document.getElementById('about_me').getBoundingClientRect().bottom)
     ){
 
         document.getElementById('hero_banner').className = "hero inactive";
         document.getElementById('my_work').className = "work inactive";
+        document.getElementById('my_photo').className = "grid-25 about-me-img active";
+        document.getElementById('my_blurb').className = "about-me-blurb grid-75 active";
+        document.getElementById('about_me_title').className = "title-active";
 
     }else{
         document.getElementById('hero_banner').className = "hero";
         document.getElementById('my_work').className = "work";
+        document.getElementById('my_photo').className = "grid-25 about-me-img";
+        document.getElementById('my_blurb').className = "about-me-blurb grid-75";
+        document.getElementById('about_me_title').className = "title-inactive";
     }
 });
 
